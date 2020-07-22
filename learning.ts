284a09d6e31248360abb621a40079e4d7881b20d
FooJamFung <gzfujianfeng@corp.netease.com>
2020-07-22 14:26:54 +0800

    update

--- learning.ts
+++ learning.ts
@@ -1,10 +1,12 @@
 interface LabeledValue {
     label: string;
+    color?: number;
+    readonly isBig: boolean; 
 }
 
 function printLabel(labeledObj: LabeledValue) {
-    console.log(labeledObj.label);
+    console.log(labeledObj);
 }
 
-let myObj = {size: 10, label: "Size 10 Object"};
+let myObj = {color: 10, label: "Size 10 Object", isBig: true};
 printLabel(myObj);
